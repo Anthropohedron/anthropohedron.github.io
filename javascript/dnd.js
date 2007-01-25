7c685694d84cda1a5daeef7ff008209648b6e9cd
Anthropohedron <Anthropohedron@users.noreply.github.com>
2007-01-25 01:23:35 +0000

    Moved dragger observes into Dragger. Reformatting.

--- javascript/dnd.js
+++ javascript/dnd.js
@@ -5,72 +5,87 @@
 
 function CardDragger(game) {
   this.game = game;
+  var self = this;
+  this.handlers = [
+      [ this.mouseDown, 'mousedown' ],
+      [ this.mouseMove, 'mousemove' ],
+      [ this.mouseUp, 'mouseup' ],
+      [ this.click, 'click' ],
+      [ this.dblClick, 'dblclick' ]
+    ];
+  this.handlers.each(function(fn)
+      { fn[0] = fn[0].bindAsEventListener(self); });
 }
 
 CardDragger.prototype = {
 
-dragCard : null,
-cX : 0,
-cY : 0,
-oldZ : 0,
+  dragCard: null,
+  cX: 0,
+  cY: 0,
+  oldZ: 0,
 
-mouseDown : function(evt) {
-              var element = Event.element(evt);
-              if (element) {
-                var dragCard =
-                  this.game.pickCard(Card.findParentCard(element), this);
-                this.dragCard = dragCard;
-                if (dragCard!=null) {
-                  dragCard.highlight(false);
-                  this.cX = Event.pointerX(evt) - dragCard.rect.left;
-                  this.cY = Event.pointerY(evt) - dragCard.rect.top;
-                  this.oldZ = dragCard.getZ();
-                  dragCard.setZ(this.oldZ + 150);
-                  //return true;
-                } else {
-                  this.dragCard = null;
-                  //return false;
-                }
-              }
-              return false;
-            },
+  observeElement: function(element) {
+    this.handlers.each(function(h) { Event.observe(element, h[1], h[0]); });
+    element = null;
+  },
 
-mouseMove : function(evt) {
-              if (this.dragCard!=null) {
-                this.dragCard.moveTo(Event.pointerX(evt) - this.cX, Event.pointerY(evt) - this.cY);
-                this.game.movedCard(evt, this.dragCard, this);
-                //return true;
-              }
-              return false;
-            },
+  mouseDown: function(evt) {
+    var element = Event.element(evt);
+    if (element) {
+      var dragCard =
+        this.game.pickCard(Card.findParentCard(element), this);
+      this.dragCard = dragCard;
+      if (dragCard!=null) {
+        dragCard.highlight(false);
+        this.cX = Event.pointerX(evt) - dragCard.rect.left;
+        this.cY = Event.pointerY(evt) - dragCard.rect.top;
+        this.oldZ = dragCard.getZ();
+        dragCard.setZ(this.oldZ + 150);
+        //return true;
+      } else {
+        this.dragCard = null;
+        //return false;
+      }
+    }
+    return false;
+  },
 
-mouseUp : function(evt) {
-            if (this.dragCard!=null) {
-              var card = this.dragCard;
+  mouseMove: function(evt) {
+    if (this.dragCard!=null) {
+      this.dragCard.moveTo(Event.pointerX(evt) - this.cX, Event.pointerY(evt) - this.cY);
+      this.game.movedCard(evt, this.dragCard, this);
+      //return true;
+    }
+    return false;
+  },
 
-              this.dragCard = null;
-              card.setZ(this.oldZ);
-              this.game.dropCard(evt, card, this);
-              //return true;
-            }
-            return false;
-          },
+  mouseUp: function(evt) {
+    if (this.dragCard!=null) {
+      var card = this.dragCard;
 
-click : function(evt) {
-          var card = Card.findParentCard(Event.element(evt));
-          if (card!=null) {
-            this.game.clickCard(card, this);
-          }
-          return false;
-        },
+      this.dragCard = null;
+      card.setZ(this.oldZ);
+      this.game.dropCard(evt, card, this);
+      //return true;
+    }
+    return false;
+  },
 
-dblClick : function(evt) {
-             var card = Card.findParentCard(Event.element(evt));
-             if (card!=null) {
-               this.game.dblclickCard(card, this);
-             }
-             return false;
-           }
+  click: function(evt) {
+    var card = Card.findParentCard(Event.element(evt));
+    if (card!=null) {
+      this.game.clickCard(card, this);
+    }
+    return false;
+  },
+
+  dblClick: function(evt) {
+    var card = Card.findParentCard(Event.element(evt));
+    if (card!=null) {
+      this.game.dblclickCard(card, this);
+    }
+    return false;
+  }
 
 };
 
--- javascript/game.js
+++ javascript/game.js
@@ -12,102 +12,92 @@ function CardGame() {
 
 CardGame.prototype = {
 
-subclass : function(json) {
-             for (property in json) {
-               this[property] = json[property];
-             }
-             return this;
-           },
+  subclass: function(json) {
+    for (property in json) {
+      this[property] = json[property];
+    }
+    return this;
+  },
 
-deck : null,
-oldLoc : null,
-newLoc : null,
-oldParent : null,
-dragStackSize : 0,
-dragger : null,
+  deck: null,
+  oldLoc: null,
+  newLoc: null,
+  oldParent: null,
+  dragStackSize: 0,
+  dragger: null,
 
-newLocation : function(x, y, dx,dy) {
-                var loc = new CardLocation(x, y, dx, dy);
-                this.locations[this.locations.length] = loc;
-                return loc;
-              },
+  newLocation: function(x, y, dx,dy) {
+    var loc = new CardLocation(x, y, dx, dy);
+    this.locations[this.locations.length] = loc;
+    return loc;
+  },
 
-activate : function() {
-             for (var i = 0; i<this.locations.length; ++i) {
-               this.locations[i].clear();
-               this.locations[i].show(true);
-             }
-             this.shuffle();
-             document.dragger = this.dragger;
-             if (this.reposition) this.reposition();
-           },
+  activate: function() {
+    for (var i = 0; i<this.locations.length; ++i) {
+      this.locations[i].clear();
+      this.locations[i].show(true);
+    }
+    this.shuffle();
+    document.dragger = this.dragger;
+    if (this.reposition) this.reposition();
+  },
 
-deactivate : function() {
-               for (var i = 0; i<this.locations.length; ++i) {
-                 this.locations[i].clear();
-                 this.locations[i].show(false);
-               }
-               this.shuffle();
-             },
+  deactivate: function() {
+    for (var i = 0; i<this.locations.length; ++i) {
+      this.locations[i].clear();
+      this.locations[i].show(false);
+    }
+    this.shuffle();
+  },
 
-constructMe : function(element) {
-                this.element = $(element);
-                this.dragger = new CardDragger(this);
-                var handlers = [
-                  [ this.dragger.mouseDown, 'mousedown' ],
-                  [ this.dragger.mouseMove, 'mousemove' ],
-                  [ this.dragger.mouseUp, 'mouseup' ],
-                  [ this.dragger.click, 'click' ],
-                  [ this.dragger.dblClick, 'dblclick' ]
-                ];
-                handlers.each(function(h) {
-                    Event.observe(element, h[1],
-                      h[0].bindAsEventListener(this.dragger));
-                    }.bind(this));
-              },
+  constructMe: function(element) {
+    this.element = $(element);
+    this.dragger = new CardDragger(this);
+    this.dragger.observeElement(this.element);
+  },
 
-redo : function() { this.actions.redo(); },
+  redo: function() { this.actions.redo(); },
 
-undo : function() { this.actions.undo(); },
+  undo: function() { this.actions.undo(); },
 
-startCardMove : function(card, depth) {
-                  this.dragStackSize = ((depth==null)||(depth<1)) ? 1 : depth;
-                  this.oldLoc = card.location;
-                },
+  startCardMove: function(card, depth) {
+    this.dragStackSize = ((depth==null)||(depth<1)) ? 1: depth;
+    this.oldLoc = card.location;
+  },
 
-                // adds the action to the actions list and returns it
-allowCardMove : function(card) {
-                  var action = null;
-                  if (this.newLoc!=null) {
-                    this.newLoc.highlight(false);
-                    if (this.oldLoc!=null) {
-                      action = new CardAction(this.oldLoc, this.newLoc, this.dragStackSize);
-                      this.actions.pushAction(action);
-                    }
-                  }
-                  return action;
-                },
+  // adds the action to the actions list and returns it
+  allowCardMove: function(card) {
+    var action = null;
+    if (this.newLoc!=null) {
+      this.newLoc.highlight(false);
+      if (this.oldLoc!=null) {
+        action = new CardAction(this.oldLoc, this.newLoc, this.dragStackSize);
+        this.actions.pushAction(action);
+      }
+    }
+    return action;
+  },
 
-rejectCardMove : function(card) {
-                   if (this.oldLoc!=null) {
-                     this.oldLoc.reposition();
-                     this.oldLoc = null;
-                   }
-                 },
+rejectCardMove: function(card) {
+    if (this.oldLoc!=null) {
+      this.oldLoc.reposition();
+      this.oldLoc = null;
+    }
+  },
 
-pickCard : function(card, dragger) { return true; },
+  pickCard: function(card, dragger) { return true; },
 
-movedCard : function(evt, card, dragger) { },
+  movedCard: function(evt, card, dragger) { },
 
-dropCard : function(evt, card, dragger) { },
+  dropCard: function(evt, card, dragger) { },
 
-clickCard : function(card, dragger) { },
+  clickCard: function(card, dragger) { },
 
-dblclickCard : function(card, dragger) { },
+  dblclickCard: function(card, dragger) { },
 
-shuffle : function() { this.deck.shuffle(); },
+  shuffle: function() { this.deck.shuffle(); },
 
-deal : function() { }
+  deal: function() { }
 
 };
 
