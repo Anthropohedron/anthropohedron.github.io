00881350df2cad07fc0b03235647c5d1cb15dd04
Anthropohedron <Anthropohedron@users.noreply.github.com>
2007-01-26 16:02:33 +0000

    Reformatting.

--- javascript/location.js
+++ javascript/location.js
@@ -19,79 +19,77 @@ function CardLocation(x, y, dx, dy, element) {
 
 CardLocation.prototype = {
 
-setHasCards : function(hasSome) {
-                if (hasSome) {
-                  this.placeholder.makeCardback();
-                } else {
-                  this.placeholder.makePlaceholder();
-                }
-              },
+  setHasCards: function(hasSome) {
+    if (hasSome) {
+      this.placeholder.makeCardback();
+    } else {
+      this.placeholder.makePlaceholder();
+    }
+  },
 
-moveTo : function(x, y) {
-           this.x = x;
-           this.y = y;
-           this.placeholder.moveTo(x, y);
-           if (this.cardStack.size()>0)
-             this.cardStack.list[0].moveTo(this.x, this.y, 1);
-         },
+  moveTo: function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.placeholder.moveTo(x, y);
+    if (this.cardStack.size()>0)
+      this.cardStack.list[0].moveTo(this.x, this.y, 1);
+  },
 
-setOffset : function(dx, dy) {
-              this.dx = dx;
-              this.dy = dy;
-              if (this.cardStack.size()>1)
-                this.cardStack.list[0].reattach(dx, dy);
-            },
+  setOffset: function(dx, dy) {
+    this.dx = dx;
+    this.dy = dy;
+    if (this.cardStack.size()>1)
+      this.cardStack.list[0].reattach(dx, dy);
+  },
 
-show : function(visible) {
-         var list = this.cardStack.list;
-         this.placeholder.show(visible);
-         for (var i = 0; i<list.length; ++i) {
-           list[i].show(visible);
-         }
-       },
+  show: function(visible) {
+    var list = this.cardStack.list;
+    this.placeholder.show(visible);
+    for (var i = 0; i<list.length; ++i) {
+      list[i].show(visible);
+    }
+  },
 
-top : function() {
-        return this.cardStack.top();
-      },
+  top: function() { return this.cardStack.top(); },
 
-pop : function() {
-        var card = this.cardStack.pop();
-        card.location = null;
-        card.detach();
-        return card;
-      },
+  pop: function() {
+    var card = this.cardStack.pop();
+    card.location = null;
+    card.detach();
+    return card;
+  },
 
-push : function(card) {
-         if (card instanceof Card) {
-           if (this.cardStack.size()==0) {
-             card.moveTo(this.x, this.y, 1);
-           } else {
-             this.top().attachCard(card, this.dx, this.dy);
-           }
-           this.cardStack.push(card);
-           card.location = this;
-         }
-       },
+  push: function(card) {
+    if (card instanceof Card) {
+      if (this.cardStack.size()==0) {
+        card.moveTo(this.x, this.y, 1);
+      } else {
+        this.top().attachCard(card, this.dx, this.dy);
+      }
+      this.cardStack.push(card);
+      card.location = this;
+    }
+  },
 
-reposition : function() {
-               if (this.cardStack.size()>0) {
-                 this.top().reposition(this.x, this.y);
-               }
-             },
+  reposition: function() {
+    if (this.cardStack.size()>0) {
+      this.top().reposition(this.x, this.y);
+    }
+  },
 
-highlight : function(lit) {
-              var card = this.cardStack.top();
-              if (card==null) {
-                card = this.placeholder;
-              }
-              card.highlight(lit);
-            },
+  highlight: function(lit) {
+    var card = this.cardStack.top();
+    if (card==null) {
+      card = this.placeholder;
+    }
+    card.highlight(lit);
+  },
 
-clear : function() {
-          while (this.cardStack.size()>0) {
-            this.cardStack.pop().setFaceUp(false);
-          }
-        }
+  clear: function() {
+    while (this.cardStack.size()>0) {
+      this.cardStack.pop().setFaceUp(false);
+    }
+  }
 
 };
 
